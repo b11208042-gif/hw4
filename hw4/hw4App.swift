d2a6bce885a458981062bd718cb20a767f6d6981
geopeper <geopeper@users.noreply.github.com>
2025-11-02 11:26:38 +0800

    Initial Commit

---
+++ hw4/hw4App.swift
@@ -0,0 +1,17 @@
+//
+//  hw4App.swift
+//  hw4
+//
+//  Created by 蘇昱齊 on 2025/11/2.
+//
+
+import SwiftUI
+
+@main
+struct hw4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hw4Tests/hw4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  hw4Tests.swift
+//  hw4Tests
+//
+//  Created by 蘇昱齊 on 2025/11/2.
+//
+
+import Testing
+@testable import hw4
+
+struct hw4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
